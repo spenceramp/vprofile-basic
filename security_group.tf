064537266cf2fca797c5f278660db7eeca769409
spenceramp <spencerampofo@gmail.com>
2024-07-17 01:01:57 -0400

    Added security groups and vpc to env

--- security_group.tf
+++ security_group.tf
@@ -1,12 +1,27 @@
 resource "aws_security_group" "elb" {
   name        = "tf-vprofile-elb"
   description = "Security group for vprofile load balancer"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.vpc.id
 
   tags = {
     Name = "tf-vprofile-elb"
   }
+ ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
+
 
 resource "aws_vpc_security_group_ingress_rule" "elb_inbound" {
   security_group_id = aws_security_group.elb.id
