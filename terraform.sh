239ba098ff863f8b10eeee68a69a31ff6e7e13fb
spencer <spencerampofo@gmail.com>
2024-09-11 21:45:34 -0400

    add terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e 
+
+
+
+echo "Using the action of: ${param.actions}"
+
+# if [ "${actions}" == "apply" ]; then
+#     'terraform ${actions}'
+# else
+#     echo "This is ${ENVIRONMENT} environment"
+# fi
+# terraform ${ action}
