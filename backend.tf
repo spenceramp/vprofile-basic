c10f89451e673993951bfac2f6be9f10988fd96a
spencer <spencerampofo@gmail.com>
2024-09-11 23:42:37 -0400

    add tf providers

--- backend.tf
+++ backend.tf
@@ -5,4 +5,17 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "statelockingterraform"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
